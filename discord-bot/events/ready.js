a13fc1eef2b4282ec19ecf6183b096693bff60d4
huihudk4333 <forrealroboffical@gmail.com>
2026-07-18 22:49:18 +1000

    Update bot activity message on ready event

--- discord-bot/events/ready.js
+++ discord-bot/events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`✅ Logged in as ${client.user.tag}`);
-    client.user.setActivity('for /ticket-panel', { type: 3 }); // 3 = Watching
+    client.user.setActivity('made by forrealrob dm him for a bot like me', { type: 3 }); // 3 = Watching
   },
 };
